6f0dd6321c554a635191d523087505e55635c5d3
ELLT <tobias.ellmauer@porscheinformatik.at>
2019-07-22 12:01:45 +0200

    test

---
+++ cucumber-report-web/target/classes/static/js/config.js
@@ -0,0 +1,8 @@
+var serverUrl = 'rest/';
+var queryBaseUrl = serverUrl + 'query/';
+var rolesBaseUrl = serverUrl + 'roles/';
+var collectionBaseUrl = serverUrl + 'collection/';
+var categoriesUrl = serverUrl + 'collection/categories';
+var productsUrl = serverUrl + 'collection/products';
+var fileBaseUrl = serverUrl + 'file/';
+var reportFileName = 'report.json';
